282c45e7262bbecbd431d6bc0e89a803bbe548a0
segundoc361 <126724727+segundoc361@users.noreply.github.com>
2025-01-30 00:45:18 +0000

    fift commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+public class Input {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // user name
+        String namestring;
+        System.out.print("Enter your name:");
+        namestring = scanner.nextLine();
+
+        // user age
+        int ageint;
+        System.out.print("Enter your age:");
+        ageint = scanner.nextInt();
+
+        // users weight
+        float weightfloat;
+        System.out.print("Enter your weight:");
+        weightfloat = scanner.nextFloat();
+
+        // user smoker question true or false
+        boolean smokerboolean;
+        System.out.print("Enter 'True' if you are a smoker otherwise enter 'false':");
+        smokerboolean = scanner.nextBoolean();
+
+
+        System.out.println("You enter your name is:" + namestring);
+        System.out.println("You enter your age is:" + ageint);
+        System.out.println("You enter your weight is:" + weightfloat);
+        System.out.println("You enter your smoker state is:" + smokerboolean);
+    }
+}
